2e043024ec9b7e0be1c581f71f55050c1719cc3c
Baran Açıkgöz <baranacikgoz_33@outlook.com>
2020-06-17 19:23:56 +0300

    asd

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 document.querySelector("#button1").addEventListener("click", getTextFile);
+document.querySelector("#button2").addEventListener("click", getJSON);
+
 
 function getTextFile(){
-    fetch("textfile.txt").then(function(response){
-        console.log(response);
+    Headers
+    fetch("https://github.com/baranacikgoz/Fetch-api/blob/master/textfile.txt").then(function(response){
+        
+        return response.json();
+
+    }).then(function(data){
+       document.getElementById("output").innerHTML += data;
+    }).catch(function(err){
+        console.log(err);
+    })
+}
+
+
+function getJSON(){
+    Headers
+    fetch("https://github.com/baranacikgoz/Fetch-api/blob/master/article.json").then(function(response){
+        
+        return response.text();
+
+    }).then(function(data){
+       document.getElementById("output").innerHTML += data;
+    }).catch(function(err){
+        console.log(err);
     })
 }
